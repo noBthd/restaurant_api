903a4ffa5fb7d9e8276b94b7df83dda66cacd33d
noBthd <Egorkir0401@gmail.com>
2025-06-22 19:04:00 +0300

    fix

--- internal/services/bill_service.go
+++ internal/services/bill_service.go
@@ -64,31 +64,31 @@ func GetBillByReservationID(reservationID int) (*models.Bill, error)  {
 	return &bill, nil
 }
 
-func PayBill(billID int) error {
+func PayBill(reservationID int) error {
 	if db.DB == nil {
 		log.Println("Database conn is not initialized")
 		return sql.ErrConnDone
 	}
 
 	query := "SELECT is_paid FROM bill WHERE reservation_id = $1"
-	row := db.DB.QueryRow(query, billID)
+	row := db.DB.QueryRow(query, reservationID)
 	var isPaid bool
 	if err := row.Scan(&isPaid); err != nil {
 		if err == sql.ErrNoRows {
-			log.Printf("No bill found for reservation ID %d\n", billID)
-			return fmt.Errorf("no bill found for reservation ID %d", billID)
+			log.Printf("No bill found for reservation ID %d\n", reservationID)
+			return fmt.Errorf("no bill found for reservation ID %d", reservationID)
 		}
 		log.Printf("Error scanning bill payment status: %v\n", err)
 		return err
 	}
 
 	if isPaid {
-		log.Printf("Bill for reservation ID %d is already paid\n", billID)
-		return fmt.Errorf("bill for reservation ID %d is already paid", billID)
+		log.Printf("Bill for reservation ID %d is already paid\n", reservationID)
+		return fmt.Errorf("bill for reservation ID %d is already paid", reservationID)
 	}
 
 	query = "UPDATE bill SET is_paid = true WHERE reservation_id = $1"
-	_, err := db.DB.Exec(query, billID)
+	_, err := db.DB.Exec(query, reservationID)
 	if err != nil {
 		log.Printf("Error updating bill payment status: %v\n", err)
 		return err
